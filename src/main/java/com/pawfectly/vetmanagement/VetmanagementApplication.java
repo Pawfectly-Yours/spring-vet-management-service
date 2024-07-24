8b8b544332ea16399e1f2978a9696d72ea49a11c
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-24 10:27:54 +0800

    first commit

---
+++ src/main/java/com/pawfectly/vetmanagement/VetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.pawfectly.vetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawfectly/vetmanagement/VetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawfectly.vetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
