a8f0a278c06d52dcf8178f349e9e45b395724a3a
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-24 10:38:41 +0800

    add config microservice and initial api test

--- src/main/java/com/pawfectly/vetmanagement/VetManagementApplication.java
+++ src/main/java/com/pawfectly/vetmanagement/VetManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VetmanagementApplication {
+public class VetManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VetmanagementApplication.class, args);
+		SpringApplication.run(VetManagementApplication.class, args);
 	}
 
 }
